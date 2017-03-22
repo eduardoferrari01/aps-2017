5899fbbc416c9439dfa22055940d5c36971bce33
Eduardo Ferrari <eduardo___ferrari@outlook.com>
2017-03-22 10:14:51 -0300

    First Commit
    Signed-off-by: eduardoferrari01 <eduardo___ferrari@outlook.com>

---
+++ src/main/java/br/com/aps/Aps2017Application.java
@@ -0,0 +1,12 @@
+package br.com.aps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Aps2017Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Aps2017Application.class, args);
+	}
+}
