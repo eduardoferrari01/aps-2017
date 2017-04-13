5d15688b0d7ca625eac6496195f7ee44aaa1223b
wandersonsantiago <wandersonsantiago@wandersonsantiago>
2017-04-12 22:43:59 -0300

    desenvolvimento do front end e configuração do projeto, banco de dados e mapeamento deas entidades

--- src/main/java/br/com/aps/Aps2017Application.java
+++ src/main/java/br/com/aps/Aps2017Application.java
@@ -1,8 +1,22 @@
 package br.com.aps;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+@Configuration
+@EnableAsync
+@EnableScheduling
+@EnableJpaRepositories
+@EnableTransactionManagement
+@ComponentScan(basePackages = {"br.com.aps"})
+@EnableAutoConfiguration
 @SpringBootApplication
 public class Aps2017Application {
 
